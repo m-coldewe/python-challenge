24c2209279abd9eb744e775bc2b764851970b408
m-coldewe <152045367+m-coldewe@users.noreply.github.com>
2024-01-06 23:28:07 -0500

    Second commit for PyBank, first for PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,76 @@
 import os #to create file paths across operating systems
-#import os.path
 import csv #to get module for reading csv files
 
 csvpath = os.path.join("Resources","budget_data.csv") #to set the location to read from
+
+def blank_space():
+   '''this function puts blank space between blocks of code for readability'''
+   print()
+   print()
 
 with open(csvpath) as csvfile: #to read the csv file with csv module
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    # data_list = []
-    # for row in csvreader:
-    #   data_list.append(row)
+
+    blank_space
+
+    data_list = []
+    for row in csvreader:
+      data_list.append(row)
 
     #total number of months
     row_count = len(data_list)
     print("Total Months: " + str(row_count))
 
-    print()
-    print() 
+    blank_space()
 
     #net total of amount for entire period
+    Total = 0
     for row in data_list:
-       amoumt= row in [row[1]]
-       Total = (sum(int(amoumt)))
-       print(Total)
+       Total += int(row[1])
+    print("Total: $" + str(Total))
+
+    blank_space()
+
+
+    #changes in profit/losses over the entire period
+    #create offset list to subtract from unaltered list
+    #split data list into dates and values
+
+
+
+    offset_list = list(data_list)
+    del (offset_list[-1])
+    offset_list.insert(1, '0')
+
+    #convert both list values to integers
+    #int_offset_list = []
+    #for row in offset_list:
+    #   int_offset_list = int(row[1])
+    #   print(int_offset_list)
+    print(offset_list)
+
+    #subtract offset list from unaltered list and save as new list
+    value_change = [list(data_list[-1])-list(offset_list[-1])]
+
+
+   # print(value_change)
+    #for number in reversed(value_change):
+       
+    
+    print(value_change)
+
+    #value_change = data_list[i]-data_list[i-1] for i in range(1, len(data_list)) 
+  
+
+    blank_space()
+
+    #find the greatest increase in profits
+    for row in data_list:  #subtract the previous month from the current month
+       value_change = end_val - start_val
+       greatest_value = max(value_change)  #find the largest change
+    
+    print(greatest_value)
+    
---
+++ PyPoll/main.py
@@ -0,0 +1,67 @@
+import os #to create file paths across operating systems
+import csv #to get module for reading csv files
+
+csvpath = os.path.join("Resources","election_data.csv") #to set the location to read from
+
+print('Election Results')
+
+with open(csvpath) as csvfile:  #to open the csvfile
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    #create list for data manipulation
+    ballot_data=[]
+    for row in csvreader:
+        ballot_data.append(row)
+    
+
+    #get total number of votes
+    vote_count = len(ballot_data)
+    print('Total Votes: ' + str(vote_count)) 
+    
+    #get the list of candidates who received votes
+    candidate_n = set()  #name set
+    for row in ballot_data:  #loop through list to check rows for unique values
+        candidate_n.add(row[2])  #add unique values to set
+       
+
+    candidate_list = list(candidate_n)  #translate set back to a list for use
+
+
+    #set starting vote for each candidate
+    can1_total = 0
+    can2_total = 0
+    can3_total = 0
+
+    #loop through rows to count number of votes received
+    for row in ballot_data:
+        if candidate_list[0] in row:
+            can1_total = can1_total + 1
+        elif candidate_list[1] in row:
+            can2_total = can2_total + 1
+        elif candidate_list[2] in row:
+            can3_total = can3_total + 1
+    
+
+    #get the percentage of votes each candidate won
+    percent_can1 = can1_total / vote_count * 100
+    percent_can2 = can2_total / vote_count * 100
+    percent_can3 = can3_total / vote_count * 100
+
+    #print the results of the voting
+    print(candidate_list[0] + ': ' + str(round(percent_can1, 3)) + '% (' + str(can1_total) + ')')
+    print(candidate_list[1] + ': ' + str(round(percent_can2, 3)) + '% (' + str(can2_total) + ')')
+    print(candidate_list[2] + ': ' + str(round(percent_can3, 3)) + '% (' + str(can3_total) + ')')
+
+    #declare the winner
+    #the winning candidate is the candidate with the most votes
+    if can1_total > can2_total and can3_total:
+        print('Winner: ' + candidate_list[0])
+    elif can2_total > can1_total and can3_total:
+        print('Winner: ' + candidate_list[1])
+    elif can3_total > can1_total and can2_total:
+        print('Winner: ' + candidate_list[2])
+        
+
+
+
