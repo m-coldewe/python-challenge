40aa6be854c7de5efbb63082dcba2c7196145e2b
m-coldewe <152045367+m-coldewe@users.noreply.github.com>
2024-01-09 00:22:54 -0500

    final commit prior to first submission, includes fix to elections_results, and complete pybank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,74 +3,88 @@ import csv #to get module for reading csv files
 
 csvpath = os.path.join("Resources","budget_data.csv") #to set the location to read from
 
-def blank_space():
-   '''this function puts blank space between blocks of code for readability'''
-   print()
-   print()
 
 with open(csvpath) as csvfile: #to read the csv file with csv module
-    csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+   csvreader = csv.reader(csvfile, delimiter=",")
+   csv_header = next(csvreader)
+   print('Financial Analysis')
 
-
-    blank_space
-
-    data_list = []
-    for row in csvreader:
+   #create list from csvreader for readability
+   data_list = []
+   for row in csvreader:
       data_list.append(row)
+   
 
-    #total number of months
-    row_count = len(data_list)
-    print("Total Months: " + str(row_count))
+   #get total number of months
+   row_count = len(data_list)
+   print("Total Months: " + str(row_count))
 
-    blank_space()
+   #get net total amount for entire period
+   Total = 0
+   for row in data_list:
+      Total += int(row[1])
+   print("Total: $" + str(Total))
 
-    #net total of amount for entire period
-    Total = 0
-    for row in data_list:
-       Total += int(row[1])
-    print("Total: $" + str(Total))
-
-    blank_space()
-
-
-    #changes in profit/losses over the entire period
-    #create offset list to subtract from unaltered list
-    #split data list into dates and values
-
-
-
-    offset_list = list(data_list)
-    del (offset_list[-1])
-    offset_list.insert(1, '0')
-
-    #convert both list values to integers
-    #int_offset_list = []
-    #for row in offset_list:
-    #   int_offset_list = int(row[1])
-    #   print(int_offset_list)
-    print(offset_list)
-
-    #subtract offset list from unaltered list and save as new list
-    value_change = [list(data_list[-1])-list(offset_list[-1])]
-
-
-   # print(value_change)
-    #for number in reversed(value_change):
-       
-    
-    print(value_change)
-
-    #value_change = data_list[i]-data_list[i-1] for i in range(1, len(data_list)) 
+   #create list for only the profit/loss column
+   current_value = []
+   for row in data_list:
+      current_value.append(row[1])
   
 
-    blank_space()
+   #create offset list to subtract from the current_value list
+   offset_value = list(current_value)
+   del(offset_value[-1])
+   offset_value.insert(0, '0')
+   
 
-    #find the greatest increase in profits
-    for row in data_list:  #subtract the previous month from the current month
-       value_change = end_val - start_val
-       greatest_value = max(value_change)  #find the largest change
-    
-    print(greatest_value)
-    
+   #use list comprehension to get the change between the values in the lists
+   for row in range(len(offset_value)):
+      value_change = [int(current_value[x]) - int(offset_value[x]) for x in range(len(offset_value))]
+   
+
+   #calculate the change in value for the entire dataset   
+   total_value_change = 0
+   for each in value_change:
+      total_value_change -= each
+  
+
+   #average the changes for the provided time period
+   average_value_change = total_value_change / row_count
+   print('Average Change: $' + str(round(average_value_change,2)))
+
+   #create separate list of dates
+   date_list = []
+   for row in data_list:
+      date_list.append(row[0])
+   
+      
+   #zip value_change list with the date list to re-pair the values
+   combined_list = list(zip(value_change, date_list))
+  
+   #find the greatest increase in profits
+   greatest_value_increase = max(combined_list)
+
+   #find the greatest decrease in profits
+   greatest_value_decrease = min(combined_list)
+
+   #print both values
+   print('Greatest Increase in Profits: $' + str(greatest_value_increase)[1:-1])
+   print('Greatest Decrease in Profits: $' + str(greatest_value_decrease)[1:-1])
+
+   
+   bank_output_path = os.path.join('Analysis','Financial_Results.txt')  #to set the out results txt file location
+
+   with open(bank_output_path, 'w') as file:  #to write the result values to the txt file
+      file.write('Financial Analysis')
+      file.write('\n''\n')
+      file.write('--------------------------------')
+      file.write('\n''\n')
+      file.write("Total Months: " + str(row_count))
+      file.write('\n''\n')
+      file.write("Total: $" + str(Total))
+      file.write('\n''\n')
+      file.write('Average Change: $' + str(round(average_value_change,2)))
+      file.write('\n''\n')
+      file.write('Greatest Increase in Profits: $' + str(greatest_value_increase)[1:-1])
+      file.write('\n''\n')
+      file.write('Greatest Decrease in Profits: $' + str(greatest_value_decrease)[1:-1])
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # python-challenge
-two Python challenges, PyBank and PyPoll.
+Contains two Python challenges: PyBank and PyPoll.
+
+# PyBank Financial Analysis
+
+## Overview
+
+This project provides a high-level overview of the institution's financial status. Program utilizes Python version 3.10.
+
+## Purpose
+
+-To summarize a large number of financial transactions.
+-Get the total number of months included in the dataset.
+-Learn the net total amount of Profit/Losses for the entire period.
+-See the month with the greatest increase in profits, as well as the amount
+-See the month with the greatest decrease in profits, as well as the amount
+
+
+When completed, the analysis will also allow you to see the average change for the included time period, as well, but the calculations are not yet ready.
+
+Note: For the code, as written, data must be submitted as csv file.
+
+## Result
+
+Financial Analysis
+
+--------------------------------
+
+Total Months: 86
+
+Total: $22564198
+
+Average Change: $-4448.13
+
+Greatest Increase in Profits: $1862002, 'Aug-16'
+
+Greatest Decrease in Profits: $-1825558, 'Feb-14'
+
+
+
+## Summary
+This code provides a glimpse of the most relevant values regarding your, or your company's, financial well-being.
+Conclusions of analysis and final words.
+
+
+# PyPoll Election Results
+
+## Overview
+
+This project provides a summary of the election results, including the final outcome of those results. Utilizes Python version 3.10.
+
+## Purpose
+
+-Get the total number of votes submitted.
+-Find out which candidates received votes.
+-See the total number of votes, and the percentage of the total vote per candidate.
+-Winner generated automatically from the results.
+
+Note: For the code, as written, data must be submitted as a csv file.
+
+## Result
+
+Election Results
+
+Diana DeGette: 73.812% (272892)
+Raymon Anthony Doane: 3.139% (11606)
+Charles Casper Stockham: 23.049% (85213)
+
+Winner: Diana DeGette
+
+---
+
+As you can see, Diana DeGette received the greatest number of votes to win the election.
+
+
+## Summary
+This code lets you see the results of the election at a glance, including all involved parties and, most importantly, the winner with supporting documentation. 
